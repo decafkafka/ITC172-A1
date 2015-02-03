423fb12bce877ca702b8d416578c0157e88e6948
decafkafka <yhan.ccs@gmail.com>
2015-02-03 11:18:08 -0800

    First Upload of Assignment

---
+++ Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="Assignment1SS.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>Age Calculator</h1>
+        <p>Enter your date of birth on the calendar and the program will determine your age in years.</p>
+        <asp:Calendar ID="Calendar1" runat="server"></asp:Calendar>
+        Today is <asp:Label ID="LblToday" runat="server" Text="Label"></asp:Label><br />
+        You are years old <asp:Label ID="LblYearsOld" runat="server" Text="Label"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //gets the current date according to the computer
+        DateTime today = DateTime.Now;
+
+        //gets the date from the calendar
+        DateTime dateChosen = Calendar1.SelectedDate;
+
+        //calculates the difference between the dates
+        TimeSpan dateDiff = today.Subtract(dateChosen);
+
+        LblToday.Text = today.ToString();
+        double years = dateDiff.Days / 365.24;
+
+        LblYearsOld.Text = years.ToString();
+    }
+    protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+    {
+
+    }
+}
